608f30fb2dcffce626abe03d546af033c7c8d875
kevwu27 <115754187+kevwu27@users.noreply.github.com>
2025-04-23 01:45:55 -0700

    added spawn mechanism

---
+++ Assets/Assets/Scripts/PrefabSpawner.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+
+public class VRPrefabSpawner : MonoBehaviour
+{
+    public GameObject prefabA;
+    public GameObject prefabB;
+    public XRRayInteractor rightRayInteractor;
+
+    public Transform left;
+    public Transform right;
+
+    public InputActionProperty buttonAAction;
+    public InputActionProperty buttonBAction;
+
+    public float spawnDistance = 10.5f;
+
+    void Update()
+    {
+        
+        if (rightRayInteractor.hasSelection) return;
+        
+        if (buttonAAction.action.WasPressedThisFrame())
+        {
+            SpawnAndSelect(prefabA);
+        }
+        else if (buttonBAction.action.WasPressedThisFrame())
+        {
+            SpawnAndSelect(prefabB);
+        }
+    }
+
+    void SpawnAndSelect(GameObject prefab)
+    {
+        Transform rayOrigin = rightRayInteractor.transform;
+        Vector3 spawnPosition = rayOrigin.position + rayOrigin.forward * spawnDistance;
+        Debug.Log(spawnPosition);
+        GameObject spawned = Instantiate(prefab, spawnPosition, Quaternion.identity);
+
+        spawned.transform.rotation = Quaternion.LookRotation(rayOrigin.forward);
+
+        // if (spawned.TryGetComponent<TwoHandManipulation>(out var behavior))
+        // {
+        //     behavior.leftController = left;
+        //     behavior.rightController = right;
+        // }
+
+        // if (spawned.TryGetComponent<IXRSelectInteractable>(out var interactable))
+        // {
+        //     var manager = rightRayInteractor.interactionManager;
+        //     manager.SelectEnter(rightRayInteractor, interactable);
+        // }
+        StartCoroutine(DelayedSelect(spawned));
+    }
+
+    System.Collections.IEnumerator DelayedSelect(GameObject spawned)
+    {
+        yield return null; // Wait one frame
+
+        if (spawned.TryGetComponent<TwoHandManipulation>(out var behavior))
+        {
+            behavior.leftController = left;
+            behavior.rightController = right;
+        }
+
+        if (spawned.TryGetComponent<IXRSelectInteractable>(out var interactable))
+        {
+            var manager = rightRayInteractor.interactionManager;
+            manager.SelectEnter(rightRayInteractor, interactable);
+        }
+    }
+
+}
